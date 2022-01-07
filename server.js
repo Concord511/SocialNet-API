c2d682c5a05fdd6dd23f88c95017892e5a9eb08c
Ian Holmes <concord511@gmail.com>
2022-01-06 21:38:01 -0600

    added server.js file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost/SocialNet-API', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`);
