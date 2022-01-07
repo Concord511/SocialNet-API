0bb70efe608ed5556592c8ac767648ba7eaffa14
Ian Holmes <concord511@gmail.com>
2022-01-06 21:33:21 -0600

    Added Reaction subdocument to Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const ThoughtSchema = new Schema(
@@ -37,4 +37,36 @@ const ThoughtSchema = new Schema(
 
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
-})
+});
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
